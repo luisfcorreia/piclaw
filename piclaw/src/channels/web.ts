9ea5488919406ed25750b2c8ed30ebcff57b571d
Rui Carmo <rui.carmo@gmail.com>
2026-03-05 23:07:03 +0000

    Add chat input history and fix passkey enrol regex

--- piclaw/src/channels/web.ts
+++ piclaw/src/channels/web.ts
@@ -732,7 +732,7 @@ export class WebChannel {
         const bytes = new Uint8Array(buffer);
         let binary = '';
         for (const b of bytes) binary += String.fromCharCode(b);
-        return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '');
+        return btoa(binary).replace(/\\+/g, '-').replace(/\\//g, '_').replace(/=+$/g, '');
       };
 
       const credentialToJSON = (cred) => ({
